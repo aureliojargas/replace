97892528dae9ad98124d097506ea66d4feef69fc
Aurelio Jargas <verde@aurelio.net>
2017-06-22 19:02:58 -0300

    Isolate command line parsing into a function

--- replace.py
+++ replace.py
@@ -23,46 +23,40 @@ import argparse
 #           outfile=<_io.TextIOWrapper name='<stdout>' encoding='UTF-8'>)
 
 
-parser = argparse.ArgumentParser(
-    description='Replaces text using string or regex matching.',
-    # epilog='Ending text.'
-)
-
-# from
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-f', '--from',      metavar='TEXT', dest='from_',
-                   help='specify the search text or regex')
-group.add_argument('-F', '--from-file', metavar='FILE',
-                   help='read the search text from this file')
-
-# to
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-t', '--to',        metavar='TEXT',
-                   help='specify the replacement text')
-group.add_argument('-T', '--to-file',   metavar='FILE',
-                   help='read the replacement text from this file')
-
-# other
-parser.add_argument('-r', '--regex', action='store_true',
-                    help='use regex matching instead of string matching')
-
-parser.add_argument('-i', '--in-place', action='store_true',
-                    help='edit files in-place')
-
-# files
-parser.add_argument(
-    'files',
-    metavar='FILE',
-    nargs='+',
-    help='input files')
-
-args = parser.parse_args()
-# print(args); sys.exit(0) # debug
-
-
 def read_file(path):
     with open(path, 'r') as myfile:
         return myfile.read()
+
+
+def parse_cmdline():
+    parser = argparse.ArgumentParser(
+        description='Replaces text using string or regex matching.')
+    # from
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-f', '--from', metavar='TEXT', dest='from_',
+                       help='specify the search text or regex')
+    group.add_argument('-F', '--from-file', metavar='FILE',
+                       help='read the search text from this file')
+    # to
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-t', '--to', metavar='TEXT',
+                       help='specify the replacement text')
+    group.add_argument('-T', '--to-file', metavar='FILE',
+                       help='read the replacement text from this file')
+    # other
+    parser.add_argument('-r', '--regex', action='store_true',
+                        help='use regex matching instead of string matching')
+
+    parser.add_argument('-i', '--in-place', action='store_true',
+                        help='edit files in-place')
+    # files
+    parser.add_argument('files', metavar='FILE', nargs='+',
+                        help='input files')
+    return parser.parse_args()
+
+
+args = parse_cmdline()
+# print(args); sys.exit(0) # debug
 
 # Set search pattern
 if args.from_file:
